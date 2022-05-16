290da554275c9a15d90c374d0d29fb33527a0f4a
ann-wangui <wanguia822@gmail.com>
2022-05-16 09:33:09 +0300

    add a message

--- config.py
+++ config.py
@@ -1,21 +1,13 @@
-
-  
-## Configuration files 
 import os
-from distutils.debug import DEBUG
 
 class Config:
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?category={}&language=en&from=2022-04-16&apiKey={}'
+    '''
+    General configuration parent class
+    '''
+    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey=3a10d7cd5abf4e089804482dab06b7bd'
+    ALL_NEWS_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey=3a10d7cd5abf4e089804482dab06b7bd'
+
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
-class ProdConfig(Config):
-    pass
-
-class DevConfig(Config):
-    DEBUG = True
-
-config_options = {
-'development':DevConfig,
-'production':ProdConfig
-}
+  
