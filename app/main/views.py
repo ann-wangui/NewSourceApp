c2174b76ab704503f1978dcc62207bda3a1e1b05
ann-wangui <wanguia822@gmail.com>
2022-05-16 10:09:54 +0300

    add all changes

--- app/main/views.py
+++ app/main/views.py
@@ -1,81 +1,37 @@
-'''
-We define views that will be rendered on our pages
-'''
-from flask import render_template,request,redirect,url_for
+#from flask import render_template,request,redirect,url_for
+#from . import main
+#from flask import render_template,request,redirect,url_for
+#from ..request import get_source,article_source,get_category,get_headlines
+from turtle import title
+from flask import render_template
 from . import main
-from ..request import *
-from .forms import *
-from ..models_articles import *
+from ..request import get_sources, get_articles
+from ..model_sources import Source 
 
 #Views
-@main.route('/')
-def home():
-    tech_articles = get_article_top_headlines('technology')
-    all_articles = get_article_everything('all')
-    all_sources = get_sources()
+@main.route('/')  #Route defining the view function.
+def index():
+    '''
+    Root function returning index/home page with data
+    '''
+#Getting the popular categories    
+    general = get_sources('general')
+    sports = get_sources('sports')
+    business = get_sources('business')
+    technology = get_sources('technology')
+    entertainment = get_sources('entertainment')
+    science = get_sources('science')
 
-    search_article = request.args.get('article_query')
+    title = 'You Have Landed on the Best Site To Get the Latest News & Updates.'
 
-    if search_article:
-        return redirect(url_for('.search',article=search_article))
+    return render_template('index.html', title=title, general=general, sports=sports, business=business, technology=technology, entertainment=entertainment, science=science) 
 
-    return render_template('index.html',tech_articles=tech_articles,all_articles=all_articles,all_sources=all_sources)
+@main.route('/<source_id>')
+def articles(source_id):
 
-@main.route('/kenya')
-def kenya():
-    kenya_articles = get_article_everything('kenya')
-
-    return render_template('kenya.html',kenya_articles=kenya_articles)
-
-@main.route('/worldnews')
-def worldnews():
-    worldnews = get_article_everything('world')
-
-    return render_template('worldnews.html',worldnews=worldnews)
-
-@main.route('/business')
-def business():
-    business_news = get_article_top_headlines('business')
-
-    return render_template('business.html',business_news=business_news)
-
-@main.route('/sports')
-def sports():
-    sports_news = get_article_top_headlines('sports')
-
-    return render_template('sports.html',sports_news=sports_news)
-
-@main.route('/health')
-def health():
-    health_news = get_article_top_headlines('health')
-
-    return render_template('health.html',health_news=health_news)
-
-@main.route('/tech')
-def tech():
-    tech_news = get_article_top_headlines('technology')
-
-    return render_template('tech.html',tech_news=tech_news)
-
-@main.route('/sources')
-def sources():
-    all_sources = get_sources()
-
-    return render_template('sources.html',all_sources=all_sources)
-
-@main.route('/search/<article>')
-def search(article):
-    searched_articles_list = article.split(" ")
-    article_name_format = "+".join(searched_articles_list)
-    searched_articles = search_article(article_name_format)
-
-    heading = article.upper()
-    
-    return render_template('search.html',searched_articles=searched_articles,heading=heading)
-
-@main.route('/source/<source_name>')
-def source(source_name):
-    article_display = get_article_by_source(source_name)
-    title = source_name.upper()
-
-    return render_template('source.html', article_display=article_display,title=title )
+    '''
+    View article page function that returns the various article details page and its data
+    '''
+    news_source = get_articles(source_id)
+    title = f'Welcome to {source_id}'
+    return render_template('news.html', title=title, name = source_id, news = news_source)
